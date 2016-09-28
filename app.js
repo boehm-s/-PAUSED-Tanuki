dc6d7d09da71d8ef65be0690ee6f088c23fd2f17
Steven BOEHM <boehm_s@etna-alternance.net>
2016-09-28 03:45:54 +0200

    ok with pm2 and the server config

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+import http from 'http';
 import app from './config/express';
-import createServer from './config/server';
+import serverConf from './config/server';
 import routes from './FRONT/routes/index';
 import apiRoutes from './API/routes/index';
 
-const server = createServer(app);
-const port = (process.env.PORT || '3000');
+
+const debug = require('debug')('es6-express-api:server');
+const server = http.createServer(app);
+const port = serverConf.normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
 
 // mount routes
 app.use('/', routes);
@@ -13,8 +18,18 @@ app.use('/api', apiRoutes);
 server.listen(port);
 console.log('server listening on port ' + port);
 
+server.on('error', err => serverConf.onError(port, err));
+server.on('listening', onListening);
 
 
+
+
+
+function onListening()  {
+    let addr = server.address();
+    let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
 
 // error handlers
 app.use((req, res, next) => {
