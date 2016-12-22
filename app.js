4f0ebaf97f3737c7b3b3fade22a66c30fa90d6d8
Steven BOEHM <boehm_s@etna-alternance.net>
2016-12-22 22:09:11 +0100

    express config should be in app.js

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-import app from './config/express';
-import createServer from './config/server';
-import routes from './FRONT/routes/index';
-import apiRoutes from './API/routes/index';
+import express		from	 'express';
+import bodyParser	from	 'body-parser';
+import logger		from	 'morgan';
+import cookieParser	from	 'cookie-parser';
+import path		from	 'path';
+import sass		from	 'node-sass-middleware';
+import createServer	from	 './config/server';
+import routes		from	 './FRONT/routes/index';
+import apiRoutes	from	 './API/routes/index';
 
-const server = createServer(app);
-const port = (process.env.PORT || '3000');
+const app	= express();
+const port	= (process.env.PORT || '3000');
+const server	= createServer(app, port);
+const frontRoot = __dirname + '/FRONT/';
+const apiRoot	= __dirname + '/API/';
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(frontRoot, 'public')));
+app.use(sass({
+    src: path.join(frontRoot, 'public/sass'),
+    dest: path.join(frontRoot, 'public/css'),
+    indentedSyntax: true,
+    sourceMap: true
+}));
+
+app.set('views', path.join(frontRoot, 'views'));
+app.set('view engine', 'jade');
+
 
 // mount routes
 app.use('/', routes);
@@ -22,16 +49,6 @@ app.use((req, res, next) => {
   err.status = 404;
   next(err);
 });
-
-if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
--- config/server.js
+++ config/server.js
@@ -1,35 +1,45 @@
 import http from 'http';
-const debug = require('debug')('LetVibe:server');
+const debug = require('debug')('es6-express-api:server');
 
-function createServer(app){
-  var server = http.createServer(app);
-//  server.on('error', onError);
-  server.on('listening', onListening);
+const createServer = (app, port) => {
+    var server = http.createServer(app);
+    server.on('error', onError);
+    server.on('listening', onListening);
 
-  function normalizePort(val) {
-    var port = parseInt(val, 10);
-    if (isNaN(port)) return val;
-    if (port >= 0)   return port;
-    return false;
-  }
+    const normalizePort = val => {
+	var port = parseInt(val, 10);
+	if (isNaN(port)) return val;
+	if (port >= 0)   return port;
+	return false;
+    };
 
-  function onError(error) {
-    if (error.syscall !== 'listen') throw error;
-    var bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+    function onError(error) {
+	if (error.syscall !== 'listen')
+	    throw error;
+	var bind = (typeof port === 'string')
+		? `Pipe ${port}`
+		: `Port ${port}`;
 
-    switch (error.code) {
-    case 'EACCES':     console.error(bind + ' requires elevated privileges'); process.exit(1); break;
-    case 'EADDRINUSE': console.error(bind + ' is already in use');            process.exit(1); break;
-    default: throw error;
-    }
-  }
+	if (error.code == 'EACCES') {
+	    console.error(bind + ' requires elevated privileges');
+	    process.exit(1);
+	} else if (error.code == 'EADDRINUSE') {
+	    console.error(bind + ' is already in use');
+	    process.exit(1);
+	} else
+	    throw error;
+    };
 
-  function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    debug('Listening on ' + bind);
-  }
+    function onListening()  {
+	var addr = server.address();
+	var bind = (typeof addr === 'string')
+		? `pipe ${addr}`
+		: `port ${addr.port}`;
 
-  return (server);
-}
+	debug('Listening on ' + bind);
+    };
+
+    return server;
+};
+
 export default createServer;
