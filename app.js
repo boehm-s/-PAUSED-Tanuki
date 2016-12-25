95ce73e62a2ba6206b54af9725e86735825c6115
Steven BOEHM <boehm_s@etna-alternance.net>
2016-12-25 20:24:27 +0100

    simple ws support

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+import ws		from	 'ws';
 import createServer	from	 './config/server';
-import app		from	 './config/express';
+import app		from	 './config/app';
 import routes		from	 './FRONT/routes/index';
 import apiRoutes	from	 './API/routes/index';
-import wsRoutes		from	 './WS/routes/index';
 
 const port	= (process.env.PORT || '3000');
 const server	= createServer(app, port);
-const expressWs = require('express-ws')(app);
-
+const wsServer	= ws.Server;
+const wss	= new wsServer({server});
 
 // mount routes
 app.use('/', routes);
 app.use('/api', apiRoutes);
-app.use('/ws', wsRoutes);
+
 
 server.listen(port);
 console.log('server listening on port ' + port);
+
+
+wss.on('connection', ws => {
+    ws.on('message', msg => {
+	console.log('received: %s', msg);
+    });
+    ws.send('something');
+});
--- config/app.js
+++ config/app.js
@@ -4,11 +4,9 @@ import logger		from	 'morgan';
 import cookieParser	from	 'cookie-parser';
 import path		from	 'path';
 import sass		from	 'node-sass-middleware';
-
 const app	= express();
 const frontRoot = __dirname + '/../FRONT/';
 const apiRoot	= __dirname + '/../API/';
-const wsRoot	= __dirname + '/../WS/';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,6 +21,8 @@ app.use(sass({
     indentedSyntax: true,
     sourceMap: true
 }));
+
+
 
 app.set('views', path.join(frontRoot, 'views'));
 app.set('view engine', 'jade');
