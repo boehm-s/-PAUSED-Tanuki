4b5ed556f5099b62ca467ecc873b961ecd8713e1
Steven BOEHM <boehm_s@etna-alternance.net>
2017-04-21 11:40:37 +0200

    config db sqlite

--- .db/sqlite/config/db.js
+++ .db/sqlite/config/db.js
@@ -1,13 +1,13 @@
 import db	from 'sqlite';
 
-let conn = null;
-
-module.exports = async (db) => {
-    if (!conn) {
-	conn = await Promise.resolve()
-	.then(() => db.open('db.sqlite', { Promise }))
+const initDB = async () => {
+    await Promise.resolve()
+	.then(() => {
+	    console.log("init db");
+	    global.db = db;
+	    return db.open('./db.sqlite', { Promise });
+	})
 	.catch(err => console.error(err.stack));
-	return conn;
-    } else
-	return conn;
 };
+
+export default {initDB};
