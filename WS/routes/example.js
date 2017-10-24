a8a221a3c603ca539ada23402bd552ab7235d4c4
Steven BOEHM <boehm_s@etna-alternance.net>
2017-10-25 00:19:59 +0200

    rm WS to focus on important features

--- WS/routes/example.js
+++
@@ -1,8 +0,0 @@
-const route = wss => {
-    wss.on('connection', ws => {
-	ws.on('message', msg => {
-	});
-    });
-};
-
-module.exports = route;
--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-import ws		from  'ws';
 import createServer	from  './config/server';
 import dbConf		from  './config/db';
 import app		from  './config/app';
 import routes		from  './FRONT/routes/index';
 import apiRoutes	from  './API/routes/index';
-import wsRoutes		from  './WS';
 
 const port	= '3000';
 const server	= createServer(app, port);
-// const wsServer	= ws.Server;
-// const wss	= new wsServer({server, path: '/'});
 
 // mount routes
 app.use('/', routes);
 app.use('/api', apiRoutes);
-// wsRoutes(wsServer, server);
 
 server.listen(port);
 console.log('server listening on port ' + port);
-
-// wss.on('connection', ws => {
-//     ws.on('message', msg => {
-// 	console.log('received: %s', msg);
-//     });
-//     ws.send('something');
-// });
